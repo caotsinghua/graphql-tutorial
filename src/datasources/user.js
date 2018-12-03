514a12573ea99118fb2457679075000927a5b2f6
caoqinghua <qinghua.cao@shanbay.com>
2018-12-03 22:44:47 +0800

    :memo: emoji commit

--- src/datasources/user.js
+++ src/datasources/user.js
@@ -1,11 +1,12 @@
-const {DataSource} =require('apollo-datasource')
-class UserApi extends DataSource{
-    constructor({store}){
-        super();
-        this.store=store;
-    }
-    initialize(config){
-        this.context=config.context;
-    }
-    
+const { DataSource } = require("apollo-datasource");
+class UserApi extends DataSource {
+  constructor({ store }) {
+    super();
+    this.store = store;
+  }
+  initialize(config) {
+    this.context = config.context;
+  }
 }
+
+module.exports = UserApi;
